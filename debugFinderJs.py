4fad2e0e083ea6e3c4b8c3aa76f4d981c5f34ca4
Chris Forbes <christopherforbes1@googlemail.com>
2015-05-19 17:01:09 +0100

    additional checks for alert, also ignores bootstrap now

--- debugFinderJs.py
+++ debugFinderJs.py
@@ -5,10 +5,17 @@ import sys, os, glob
 for root, dirs, files in os.walk(sys.argv[1]):
 	for file in files:
 		if file.endswith(".js"):
-			count = 0
-			x = open(root+"/"+file, "r")
-			for line in x:
-				count += 1
-				if "debugger" in line:
-					print "debugger in "+ str(file) +" on line " + str(count)
-			x.close()
+			if "bootstrap" not in file:
+				count = 0
+				x = open(root+"/"+file, "r")
+				for line in x:
+					count += 1
+					if "debugger" in line:
+						print "debugger in "+ str(file) +" on line " + str(count)
+						print "full line" + str(line)
+					if "alert" in line:
+						if "bootbox" not in line:
+							if "//" not in line:
+								print "alert in like "+ str(file) +" on line " + str(count)
+								print "full line" + str(line)
+				x.close()
